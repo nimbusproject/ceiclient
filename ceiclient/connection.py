a5e901898a113a0f5a5f156a6af7175908078a9a
Patrick Armstrong <patricka@uvic.ca>
2012-02-07 09:59:24 -0800

    Allow specifying the dashi exchange on the command line

--- ceiclient/connection.py
+++ ceiclient/connection.py
@@ -11,16 +11,17 @@ class DashiCeiConnection(CeiConnection):
 
     _name = 'ceiclient'
 
-    def __init__(self, broker, username, password):
+    def __init__(self, broker, username, password, exchange=None):
         self.amqp_broker = broker
         self.amqp_username = username
         self.amqp_password = password
         self.amqp_port = 5672
+        self.amqp_exchange = exchange or DEFAULT_EXCHANGE
 
         self.dashi_connection = DashiConnection(self._name,
                 'amqp://%s:%s@%s:%s//' % (self.amqp_username,
                     self.amqp_password, self.amqp_broker,
-                    self.amqp_port), DEFAULT_EXCHANGE)
+                    self.amqp_port), self.amqp_exchange)
 
     def call(self, service, operation, timeout=5, **kwargs):
         return self.dashi_connection.call(service, operation, timeout, **kwargs)
