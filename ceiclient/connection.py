3563dbda7420edbe0d3ae9060b80c69feb18ddf1
Pierre Riteau <priteau@ci.uchicago.edu>
2012-02-23 00:22:53 +0100

    Add a configurable timeout

--- ceiclient/connection.py
+++ ceiclient/connection.py
@@ -11,20 +11,24 @@ class DashiCeiConnection(CeiConnection):
 
     _name = 'ceiclient'
 
-    def __init__(self, broker, username, password, exchange=None):
+    def __init__(self, broker, username, password, exchange=None, timeout=None):
         self.amqp_broker = broker
         self.amqp_username = username
         self.amqp_password = password
         self.amqp_port = 5672
         self.amqp_exchange = exchange or DEFAULT_EXCHANGE
+        self.timeout = timeout
 
         self.dashi_connection = DashiConnection(self._name,
                 'amqp://%s:%s@%s:%s//' % (self.amqp_username,
                     self.amqp_password, self.amqp_broker,
                     self.amqp_port), self.amqp_exchange)
 
-    def call(self, service, operation, timeout=5, **kwargs):
-        return self.dashi_connection.call(service, operation, timeout, **kwargs)
+    def call(self, service, operation, **kwargs):
+        return self.dashi_connection.call(service, operation, self.timeout, **kwargs)
+
+    def fire(self, service, operation, **kwargs):
+        return self.dashi_connection.fire(service, operation, **kwargs)
 
     def disconnect(self):
         self.dashi_connection.disconnect()
