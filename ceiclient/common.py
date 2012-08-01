08332c216b3d03f231e107618b80e49456631aab
Patrick Armstrong <patricka@uvic.ca>
2012-08-01 11:14:04 -0700

    Add missing file from previous commit

--- ceiclient/common.py
+++ ceiclient/common.py
@@ -29,6 +29,10 @@ def load_cloudinitd_db(run_name):
                     vars['rabbitmq_exchange'] = svc.get_attr_from_bag("rabbitmq_exchange")
                 except ConfigException:
                     vars['rabbitmq_exchange'] = None
+                try:
+                    vars['coi_services_system_name'] = svc.get_attr_from_bag("coi_services_system_name")
+                except ConfigException:
+                    vars['coi_services_system_name'] = None
             except Exception, e:
                 raise
 
