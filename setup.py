7cefe0c9d0f4f18769d2c771335a914393972deb
Patrick Armstrong <patricka@uvic.ca>
2013-01-17 12:42:41 -0800

    Add download and proper version

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@
 from distutils.core import setup
 from setuptools import setup, find_packages
 
+VERSION = '0.1.0'
+
 tests_require = ['nose', 'mock']
 setup(name='ceiclient',
-      version='0.1',
+      version=VERSION,
       description='Client tools for the OOI Common Execution Infrastructure',
       license='Apache 2.0',
       author='Nimbus team',
@@ -19,4 +21,6 @@ setup(name='ceiclient',
       install_requires=['cloudinitd>=1.2', 'dashi>=0.2.1', 'Jinja2', 'PyYAML',],
       tests_require=tests_require,
       extras_require={'test': tests_require},
+      url="http://github.com/nimbusproject/ceiclient",
+      download_url="http://www.nimbusproject.org/downloads/ceiclient-%s.tar.gz" % VERSION,
       test_suite='nose.collector')
